fa33963344ed13ad27d18775fcb57e26b06216fb
jiha <jihakim@outlook.com>
2026-04-02 17:26:58 -0400

    feat(api): expose main functionality in package root

--- src/polar_decomposition/__init__.py
+++ src/polar_decomposition/__init__.py
@@ -1,1 +1,6 @@
-# Minimal polar decomposition benchmark package.
+from .algorithms.dwh2 import dwh2
+from .algorithms.pe5 import pe5
+from .utils.normalization import normalize_matrix
+from .utils.precond import PolarResult
+
+__all__ = ["dwh2", "pe5", "normalize_matrix", "PolarResult"]

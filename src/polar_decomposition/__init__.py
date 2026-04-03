c35db9b245304b24049c8a2906f29ff58b0cda23
jiha <jihakim@outlook.com>
2026-04-03 16:53:17 -0400

    chore: remove pe5 exports and update internal package structure

--- src/polar_decomposition/__init__.py
+++ src/polar_decomposition/__init__.py
@@ -1,4 +1,4 @@
-from .algorithms import dwh2, pe5
+from .algorithms import dwh2
 from .utils import NormalizationInfo, PolarResult, normalize_matrix
-
-__all__ = ["NormalizationInfo", "PolarResult", "dwh2", "normalize_matrix", "pe5"]
+ 
+__all__ = ["NormalizationInfo", "PolarResult", "dwh2", "normalize_matrix"]
--- src/polar_decomposition/algorithms/__init__.py
+++ src/polar_decomposition/algorithms/__init__.py
@@ -1,13 +1,10 @@
 """Core polar-decomposition algorithms."""
-
-from .dwh2 import dwh2, dwh_coefficients
-from .pe5 import PAPER_MUON_ELL, PAPER_NORM_EPS, pe5, pe5_coefficients
-
+ 
+from .dwh2 import PAPER_MUON_ELL, PAPER_NORM_EPS, dwh2, dwh_coefficients
+ 
 __all__ = [
     "PAPER_MUON_ELL",
     "PAPER_NORM_EPS",
     "dwh2",
     "dwh_coefficients",
-    "pe5",
-    "pe5_coefficients",
 ]
